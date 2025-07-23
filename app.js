2e11167db79e7d967c07eeadae706bf984e6b415
Suppp21T <taylicheng0.21@gmail.com>
2025-07-23 12:13:33 +0800

    Update app.js
    Deleted line 319 "======"
    Was causing Render deployment issues

--- app.js
+++ app.js
@@ -316,7 +316,6 @@ app.get('/deleteProduct/:id', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
-=======
 const express = require('express');
 const mysql = require('mysql2');
  
