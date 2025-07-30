c886a9a4d6a373ff10b8bef3ce0160991159aa3c
Suppp21T <taylicheng0.21@gmail.com>
2025-07-30 11:44:24 +0800

    Added port in app.js

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ const connection = mysql.createConnection({
     host: 'mfk1th.h.filess.io',
     user: 'C237Team39_identitydo',
     password: 'bad27c54c3cf6aa4677445bd8ce2f7effe5ed2d1!',
-    database: 'C237Team39_identitydo'
+    database: 'C237Team39_identitydo',
+    port:'61002'
 });
 
 
